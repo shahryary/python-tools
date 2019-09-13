bfd6d689d28d5ed5771fe21674f8ff11722d34cf
Yadi <shahryary@gmail.com>
2019-09-13 16:03:25 +0200

    updated attributes

--- hdfToCsv/hdfTocsv.py
+++ hdfToCsv/hdfTocsv.py
@@ -5,31 +5,57 @@
 import h5py
 import numpy as np
 
+def format(data):
+
+    if (data.dtype == '|S5') | (data.dtype == '|S1'):
+        data = data.astype(np.int)
+    if data.dtype == 'float64':
+        csvfmt = '%.18e'
+    elif data.dtype == 'int64':
+        csvfmt = '%d'
+    else:
+        csvfmt = '%s'
+    return(csvfmt,data)
+
+
 def  hdfTocsv(filename, outdir):
     
     try:
-        h5f = h5py.File(filename, 'r')
-        a_dset_keys = list(h5f.keys())
-
-        for dset in a_dset_keys :
-            data = h5f[dset][()]
-            if data.ndim == 0 :
+        f = h5py.File(filename, 'r')
+        a_dset_keys = list(f.keys())
+        for dset in a_dset_keys:
+            data = f[dset][()]
+            if data.ndim == 0:
                 val = data[()]
                 data = np.array([val])
 
             print ('dataset=', dset)
-            
-            if data.dtype == '|S5':
-                data=data.astype(np.int)
-            if data.dtype == 'float64' :
-                csvfmt = '%.18e'
-            elif data.dtype == 'int64' :
-                csvfmt = '%d'
-            else:
-                csvfmt = '%s'
+            res = format(data)
+            csvfmt = res[0]
+            data = res[1]
+            np.savetxt(outdir+'/out_val_'+ dset+ '.csv', data, fmt = csvfmt, delimiter = ',')
 
-            np.savetxt(outdir+'/output_'+ dset+ '.csv', data, fmt = csvfmt, delimiter = ',')
+            #print attribute
+            g = f[dset]
+            if g.attrs.keys():
+                listAtt = g.attrs.keys()
+                for att in listAtt:
+                    res = g.attrs[att]
+                    if not isinstance(res, np.ndarray):
+                        val=res
+                        res=np.array([val])
+
+                    res=format(res)
+                    fmt = res[0]
+                    da = res[1]
+                    np.savetxt(outdir + '/out_attr_' + dset + "_" + att + '.csv', da, fmt=fmt, delimiter=',')
+
             print("Exported all the data-sets in: ", outdir)
     except Exception as e:
         #logging.error(traceback.format_exc())
         print(e.message)
+
+file="/mnt/jlabDISK/hd5format_convert/1001_SNP_MATRIX/imputed_snps_binary.hdf5"
+out="/mnt/jlabDISK/hd5format_convert/1001_SNP_MATRIX/output"
+
+hdfTocsv(file,out)
